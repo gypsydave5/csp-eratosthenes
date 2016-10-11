28b22ee89cf14ea4bd5f24741aafd7a50295a71f
David Wickes <dave.wickes@gmail.com>
2016-10-11 22:29:48 +0100

    dynamically generate output channels

--- eratosthenes.go
+++ eratosthenes.go
@@ -1,4 +1,4 @@
-// Package eratosthenes implements the Sieve of Eratosthenes
+// Package cspEratosthenes implements the Sieve of Eratosthenes
 // (https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes)
 // using the Communicating Sequential Processes example given in the
 // 'Bell Labs and CSP Threads' article by Russ Cox
@@ -6,46 +6,42 @@
 package cspEratosthenes
 
 // Eratosthenes calculates the first n prime numbers.
+//
+// In this version, the function that processes the first channel dynamically
+// generates any extra channels needed to fill up the silce of primes.
 func Eratosthenes(n int) (primes []int) {
-	//'n+1' as we need an additional channel to put rejected ints in.
-	var pchan = make(chan int)
-	var chans = make([]chan int, n+1)
+	var primesChannel = make(chan int)
+	var firstChan = make(chan int)
 
-	// we need to make each chan, else they are 'nil'.
-	for i := range chans {
-		chans[i] = make(chan int)
+	go accumulate(primesChannel, &primes)
+	go process(firstChan, primesChannel)
+
+	for i := 2; len(primes) < n; i++ {
+		firstChan <- i
 	}
-
-	// kick off the processing logic between the channels with go routines.
-	for i := 0; i < len(chans)-1; i++ {
-		go process(chans[i], chans[i+1], pchan)
-	}
-
-	// accumulate the primes into the slice
-	go accumulate(pchan, &primes)
-
-	// keep putting ints into the first channel until we have a full primes slice.
-	for i := 2; len(primes) != n; i++ {
-		chans[0] <- i
-	}
-
 	return primes
 }
 
-// process picks up the first number from the left hand channel, adds it to the
-// primes slice, and then checks whether every subsequent number from the left
-// channel is divisible by the first prime. If it's not it sends it to the right
-// channel
+// This time process only takes the input slice, and only generates an output
+// slice when it needs one. Process is called recursively on the output slice,
+// again using a go routine
 //
 // As per https://swtch.com/~rsc/thread/sieve.gif
-func process(left chan int, right chan int, primes chan int) {
-	p := <-left
+func process(input chan int, primes chan int) {
+	var output chan int
+	var p = <-input
+
 	primes <- p
 
 	for {
-		x := <-left
+		x := <-input
+
 		if x%p != 0 {
-			right <- x
+			if output == nil {
+				output = make(chan int)
+				go process(output, primes)
+			}
+			output <- x
 		}
 	}
 }
